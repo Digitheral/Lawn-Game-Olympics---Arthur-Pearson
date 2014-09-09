902c60347ada22e4f74131c28711e7f61d054f1e
Digitheral <digitheral@gmail.com>
2014-09-09 13:35:36 -0400

    Second version. Skeleton of program

--- LawnGameOlympics/src/com/company/Main.java
+++ LawnGameOlympics/src/com/company/Main.java
@@ -8,14 +8,44 @@ import java.util.Scanner;
 public class Main
 {
     // Main Method for Splash Screen
-    public static void SplashScreen(String[] args)
+    public static void main(String[] args)
     {
         // Prints Splash Screen
         System.out.println("Smith Family Lawn Olympic Games");
         // Initializes input
-        Scanner input = new Scanner(System.in);
-        String inString;
-        inString = in.NextLine();
+        Scanner userIn = new Scanner(System.in);
+        System.out.println("Please input a command: ");
+        // Accepts user input and makes decisions
+        String inString = userIn.nextLine();
+        if(inString.toLowerCase() == "e" || inString.toLowerCase()== "events")
+        {
+            events(); // method call to event listing
+        }
+        else if(inString.toLowerCase() == "o" || inString.toLowerCase() == "olympians")
+        {
+            olympians(); // method call to olympians
+        }
+        else if(inString.toLowerCase() == "h" || inString.toLowerCase() == "help")
+        {
+            help(); // method call to help
+        }
+        else
+        {
+            // error return
+        }
+
+    }
+    public static void events()
+    {
+    //filler
+    }
+    public static void olympians()
+    {
+    // filler
+    }
+    public static void help()
+    {
+    // filler
     }
 
 }
