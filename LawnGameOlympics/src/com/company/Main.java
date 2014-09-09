7acd45f0d1f7a7916ad00c18bf22f3ea39da4ae7
Digitheral <digitheral@gmail.com>
2014-09-09 12:56:46 -0400

    First version. Just display and simple Scanner

---
+++ LawnGameOlympics/src/com/company/Main.java
@@ -0,0 +1,21 @@
+// Program: Project I - Lawn Olympic Games
+// Arthur Pearson
+// CMPT 220 - Fall 2014 Software Development 1
+
+package com.company;
+import java.util.Scanner;
+
+public class Main
+{
+    // Main Method for Splash Screen
+    public static void SplashScreen(String[] args)
+    {
+        // Prints Splash Screen
+        System.out.println("Smith Family Lawn Olympic Games");
+        // Initializes input
+        Scanner input = new Scanner(System.in);
+        String inString;
+        inString = in.NextLine();
+    }
+
+}
